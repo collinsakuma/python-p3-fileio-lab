d92a76070a7dd86e9ec747bb57cd8645a11a83b9
Collin Sakuma <collinmsakuma@gmail.com>
2023-03-16 19:42:28 -0700

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    with open(file_name + '.txt', mode='w', encoding='utf-8') as file_name:
+        file_name.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(file_name + '.txt', mode='a', encoding='utf-8') as file_name:
+        file_name.write(append_content)
 
 def read_file(file_name):
-    pass
+    file_to_read = open(file_name + '.txt', encoding='utf-8')
+    return file_to_read.read()
+
+        # with open(file_name + '.txt') as text_file:
+        # return text_file.read()
